3059f6dfc0c90be803e6f01b0d8ba1f0b65d3587
huangqing <huangqing@qeeniao.com>
2018-11-07 17:56:17 +0800

    新增table拆安

--- bootstrap/js/admin.js
+++ bootstrap/js/admin.js
@@ -2,6 +2,15 @@ $("#userMan").click(function() {
 	directRoutersToUserList();
 });
 
+// 请求服务
+function postRequest(uri, parameters, callback) {
+	$.ajax({url : uri, data : parameters, success : function(result){
+		if (callback !=undefined) {
+			callback(result);
+		}
+	}});
+}
+
 
 function configureListener(fn) {
 	if (fn != undefined) {
@@ -42,6 +51,44 @@ var CodeZComponents = {
 			if (data.collapse) {
 				domObj.attr("data-toggle", "collapse");
 			}
+		}
+	},
+
+	// 配置Html元素
+	/*
+		id : '',
+		value : '',
+		css : '',
+		style : '',
+		data : '绑定数据',
+		name : '元素name属性名',
+		collapse : true/false,
+		html : '' e.g div, ul, li, a
+	*/
+	addHtmlDom : function(data, fn) {
+		if (data != undefined) {
+			if (data.html == undefined) {
+				return;
+			}
+			var htmlDomCode = "<" + data.html + "></" + data.html + ">";
+
+			var htmlDomObj = $(htmlDomCode);
+
+			if (data.id != undefined) {
+				htmlDomObj.attr("id", data.id);
+			}
+
+			if (data.value != undefined) {
+				htmlDomObj.prepend(data.value);
+			}
+
+			this.configureDecoration(htmlDomObj, data);
+
+			if (fn != undefined) {
+				htmlDomObj.append(fn());
+			}
+
+			return htmlDomObj;
 		}
 	},
 
@@ -268,7 +315,7 @@ var CodeZComponents = {
 	sideBar: function(divDomId, data) {
 		var navTabs = {
 			"node": {
-				"css": "nav nav-default nav-stacked"
+				"css": "nav navbar-pills nav-stacked"
 			},
 			"child": [{
 					"node": {
@@ -392,6 +439,8 @@ var CodeZComponents = {
 		configureListener(function() {
 			$("a[name='navTitle']").on("click", function(){
 				console.info($(this).data("bindData"));
+				$(".active").removeClass("active");
+				$(this).parent().attr("class", "active");
 			});
 
 			$("a[name='toggled']").on("click", function() {
@@ -407,8 +456,68 @@ var CodeZComponents = {
 	},
 
 
+	// 表格数据
+	/*
+		{
+			header : ["titleOne", "titleTwo"],
+			tbody : [{}],
+			action : ["edit", "add", "delete"]
+		}
+	*/
 	table : function(parentDom, data) {
-		
+		// table
+		var tableDom = this.addHtmlDom({
+			css : "table table-hover table-striped",
+			html : "table",
+			data : "dfdf",
+			id : "tableOne"
+		});
+
+		// header
+		var headerDom = this.addHtmlDom({
+			html : "thead"
+		});
+		var headerTrDom = this.addHtmlDom({
+			html : "tr"
+		});
+		headerDom.append(headerTrDom);
+
+		for (var i = 0; i < 4; i++) {
+			var thDom = this.addHtmlDom({
+				html : "th",
+				value : "第" + i + "标题"
+			});
+
+			headerTrDom.append(thDom);
+		}
+
+		tableDom.append(headerDom);
+
+		// content
+		var tbodyDom = this.addHtmlDom({
+			html : "tbody"
+		});
+
+		// tr
+		for (var i = 0; i < 8; i++) {
+			var trDom = this.addHtmlDom({
+				html : "tr"
+			});
+
+			for (var j = 0; j < 4; j++) {
+				var tdDom = this.addHtmlDom({
+					html : "td",
+					value : "第" + j + "个"
+				});
+
+				trDom.append(tdDom);
+			}
+			tbodyDom.append(trDom);
+		}
+
+		tableDom.append(tbodyDom);
+
+		parentDom.append(tableDom);
 	},
 
 	/*
@@ -422,8 +531,14 @@ var CodeZComponents = {
 	*/
 	footerPager : function(parentDom, data) {
 		var domName = "pagerCounter";
+		var numberPage = data.totalPage;
+		var currentIndex = data.currentIndex;
+		var perCount = data.perCount;
+		var totalCount = data.totalCount;
+
 		var pageObj = this.addUl({
-			css : "pager"
+			css : "pagination pull-right",
+			style : "margin-right : 50px;"
 		});
 		parentDom.append(pageObj);
 
@@ -436,17 +551,18 @@ var CodeZComponents = {
 
 		pageObj.append(lastPage);
 
-		var numberPage = data.totalPage;
-		var currentIndex = data.currentIndex;
-		var perCount = data.perCount;
-		var totalCount = data.totalCount;
 		for (var i = 0; i < numberPage; i++) {
 			var currentCount = (currentIndex + 1) * perCount >= totalCount ? totalCount - (currentIndex + 0) * perCount : perCount;
 			var data = {name : domName, value : i + 1, data : {"count" : currentCount}};
+			var indexPage;
 			if (currentIndex == i) {
-				data["css"] = "active";
+				indexPage = this.addLi({
+						css : "active"
+				});
+			}else {
+				indexPage = this.addLi();
 			}
-			var indexPage = this.addLi();
+
 			var indexHref = this.addHref(data);
 			indexPage.append(indexHref);
 
@@ -468,8 +584,250 @@ var CodeZComponents = {
 			var domHtm = "a[name='" + domName + "']";
 			$(domHtm).on("click", function() {
 				console.info($(this).data("bindData"));
+				$(".active").removeClass("active");
+				$(this).parent().attr("class", "active");
 			});
 		});
+	},
+
+	// 表格 + 分页
+	/*
+		{
+			url : "request url",
+			parameters : {}, 请求参数
+			totalCount : 总条数,
+			perCount : 每页的个数,
+			data : 所有数据,
+			pages : 总共几页,
+			currentPage : 当前页数
+		}
+	*/
+	tableComponents : function(data, callback){
+		// 数据源配置
+		var DataSource = function() {
+		}
+		// 数据源获取
+		if (data == undefined) { return; }
+		if (data.url == undefined) { return; }
+		postRequest(data.url, data, function(result) {
+
+			DataSource.data = result;
+			if (callback) {callback(result);}
+		});
+
+		DataSource.currentPage = 0;
+		DataSource.data = [{"data" : "title"}];
+		DataSource.perCount = 10;
+		DataSource.pages = Math.ceil(DataSource.data.length / DataSource.perCount);
+		DataSource.totalCount = DataSource.data.length;
+	},
+
+	// 表格插件
+	tablePlugins : function(parentDom) {
+
+		/*
+		classes: 'table table-hover',
+        sortClass: undefined,
+        locale: undefined,
+        height: undefined,
+        undefinedText: '-',
+        sortName: undefined,
+        sortOrder: 'asc',
+        sortStable: false,
+        rememberOrder: false,
+        striped: false,
+        columns: [[]],
+        data: [],
+        totalField: 'total',
+        dataField: 'rows',
+        method: 'get',
+        url: undefined,
+        ajax: undefined,
+        cache: true,
+        contentType: 'application/json',
+        dataType: 'json',
+        ajaxOptions: {},
+        queryParams: function (params) {
+            return params;
+        },
+        queryParamsType: 'limit', // undefined
+        responseHandler: function (res) {
+            return res;
+        },
+        pagination: false,
+        onlyInfoPagination: false,
+        paginationLoop: true,
+        sidePagination: 'client', // client or server
+        totalRows: 0, // server side need to set
+        pageNumber: 1,
+        pageSize: 10,
+        pageList: [10, 25, 50, 100],
+        paginationHAlign: 'right', //right, left
+        paginationVAlign: 'bottom', //bottom, top, both
+        paginationDetailHAlign: 'left', //right, left
+        paginationPreText: '&lsaquo;',
+        paginationNextText: '&rsaquo;',
+        search: false,
+        searchOnEnterKey: false,
+        strictSearch: false,
+        searchAlign: 'right',
+        selectItemName: 'btSelectItem',
+        showHeader: true,
+        showFooter: false,
+        showColumns: false,
+        showPaginationSwitch: false,
+        showRefresh: false,
+        showToggle: false,
+        showFullscreen: false,
+        smartDisplay: true,
+        escape: false,
+        minimumCountColumns: 1,
+        idField: undefined,
+        uniqueId: undefined,
+        cardView: false,
+        detailView: false,
+		*/
+		parentDom.bootstrapTable({
+			cache: true,
+			classes: 'table table-hover table-striped',
+			rowStyle : function(row, index) {
+				if (row.status == 1 || row.status == "1") {
+					return {classes : "success"};
+				}else {
+					return {classes : "danger"};
+				}
+				// return {};
+			},
+			search: true,
+			searchOnEnterKey: true,
+			sidePagination : "client",
+			striped: true,
+			toolbar : "#toolbar",
+			pageNumber : 1,
+			pageSize : 5,
+			pagination: true,
+			paginationLoop: false,
+			paginationPreText: "<span class=\"glyphicon glyphicon-chevron-left\"></span>",
+			paginationNextText: '<span class=\"glyphicon glyphicon-chevron-right\"></span>',
+			columns : [{
+				field : "id",
+				title : "用户ID",
+				width : 1000
+			},{
+				field : "account",
+				title : "账号",
+				width : 1000
+			},{
+				field : "userName",
+				title : "用户",
+				width : 1000
+			},{
+				field : "status",
+				title : "状态",
+				align : "center",
+				sortable : true,
+				formatter : function(value, row, index) {
+					if (value == "1" || value == 1) {
+						return "<a href=\"#\" class=\"tooltip-show\" data-toggle=\"tooltip\" title=\"已激活\"><span class= \"fa fa-check fa-fw text-success\"></span></a>";
+					}else {
+						return "<a href=\"#\" class=\"tooltip-show\" data-toggle=\"tooltip\" title=\"未激活\"><span class= \"fa fa-ban fa-fw text-warning\"></span></a>";
+					}
+				},
+				width : 1000
+			},{
+				field : "action",
+				title : "操作",
+				width : 500,
+				formatter : function(value, row, index) {
+					if (row.status == "1" || row.status == 1) {
+						return "<div class=\"row\">" +
+							"<div class=\"col-md-12\">"+
+							"<a href=\"#\" class=\"tooltip-show edit\" data-toggle=\"tooltip\" title=\"修改\"><span class=\"fa fa-pencil fa-fw\"></span></a>"+
+							"<a href=\"#\" class=\"tooltip-show cancel\" data-toggle=\"tooltip\" title=\"注销\"><span class=\"fa fa-user-times text-danger fa-fw\"></span></a>"+
+							"</div></div>";
+					}
+					return "<div class=\"row\">" +
+						"<div class=\"col-md-12\">"+
+						"<a href=\"#\" class=\"tooltip-show edit\" data-toggle=\"tooltip\" title=\"修改\"><span class=\"fa fa-pencil fa-fw\"></span></a>"+
+						"<a href=\"#\" class=\"tooltip-show activited\" data-toggle=\"tooltip\" title=\"启用\"><span class=\"fa fa-circle-o-notch fa-fw text-success\"></span></a>"+
+						"</div></div>";
+				},
+				events : {
+					"click .edit" : function(e, value, row, index) {
+						console.info("编辑");
+					},
+					"click .cancel" : function(e, value, row, index) {
+						console.info("注销");
+					},
+					"click .activited" : function(e, value, row, index) {
+						console.info("启用");
+					}
+				}
+			}],
+			data : [{
+				id : 1,
+				account : "admin01",
+				userName : "12",
+				status : "1",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "dfw",
+				status : "0",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "yahoo",
+				status : 0,
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "no",
+				status : 1,
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "khzo",
+				status : "0",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "khzo",
+				status : "0",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "khzo",
+				status : "0",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "khzo",
+				status : "0",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "khzo",
+				status : "激活",
+				action : ""
+			},{
+				id : 1,
+				account : "admin01",
+				userName : "khzo",
+				status : "激活",
+				action : ""
+			}]
+		});
+
+
 	}
 }
 
